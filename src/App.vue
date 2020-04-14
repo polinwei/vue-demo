e2dfdb24e9a3b3ae8482fcd6e4f4559f2a94fd42
polin.wei <polin.wei@gmail.com>
2020-04-14 17:02:59 +0800

    調整顯示

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       <div class="col-sm-6">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
       </div>
-      <div class="col-sm-4">
+      <div class="col-sm-5">
         <elementForm />
       </div>
     </div>
