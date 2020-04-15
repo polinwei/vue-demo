c2084b08500ca0054dbc28903184fd3ffd5a7540
polin.wei <polin.wei@gmail.com>
2020-04-15 11:37:01 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 if (process.env.NODE_ENV === 'production') {
   console.log(`Webpack is running as ${process.env.NODE_ENV} mode.`);
   module.exports = {
-    publicPath: "/",
+    publicPath: "/vue-demo/",
   }
   
 } else {
   console.log(`Webpack is running as ${process.env.NODE_ENV} mode.`)
   module.exports = {
     runtimeCompiler: true,
-    publicPath: '/vue-demo/',
+    publicPath: '/',
   }
 }
